deea5eaefc95458859dbdfdf07f718e65c3bb8de
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-05-29 00:12:43 +0100

    [Sandeep].add Order Entity

--- src/main/java/com/bridgelabz/bookstore/entity/Order.java
+++ src/main/java/com/bridgelabz/bookstore/entity/Order.java
@@ -1,5 +1,66 @@
 package com.bridgelabz.bookstore.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+
 public class Order {
 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long orderId;
+	
+	private LocalDateTime orderTime;
+	
+	private Long quantityOfBooks;
+	
+	@ManyToMany
+	private List<Book> BooksList;
+
+	public Order(Long orderId, LocalDateTime orderTime, Long quantityOfBooks, List<Book> booksList) {
+		this.orderId = orderId;
+		this.orderTime = orderTime;
+		this.quantityOfBooks = quantityOfBooks;
+		BooksList = booksList;
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDateTime getOrderTime() {
+		return orderTime;
+	}
+
+	public void setOrderTime(LocalDateTime orderTime) {
+		this.orderTime = orderTime;
+	}
+
+	public Long getQuantityOfBooks() {
+		return quantityOfBooks;
+	}
+
+	public void setQuantityOfBooks(Long quantityOfBooks) {
+		this.quantityOfBooks = quantityOfBooks;
+	}
+
+	public List<Book> getBooksList() {
+		return BooksList;
+	}
+
+	public void setBooksList(List<Book> booksList) {
+		BooksList = booksList;
+	}
 }
