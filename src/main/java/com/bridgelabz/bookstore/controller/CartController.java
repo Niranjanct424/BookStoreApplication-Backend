30c6a6485b95eded60b510701bfe344a8ba08188
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-05-31 21:12:19 +0100

    [Sandeep].added  quantity of book sum of price api

--- src/main/java/com/bridgelabz/bookstore/controller/CartController.java
+++ src/main/java/com/bridgelabz/bookstore/controller/CartController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.bookstore.dto.CartDto;
 import com.bridgelabz.bookstore.entity.CartItem;
 import com.bridgelabz.bookstore.response.Response;
 import com.bridgelabz.bookstore.service.ICartService;
@@ -50,5 +54,14 @@ public class CartController {
 		    int cartdetails = cartService.getCountOfBooks(token);
 		    return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("Count of book in cart", 200,cartdetails));
 	}
+	
+	@PutMapping(value="/booksquantity")
+	public ResponseEntity<Response> addBooksQuantityToCart(@RequestHeader String token,@RequestParam Long bookId,
+			@RequestBody CartDto bookQuantityDetails) throws Exception {
+		CartItem cartdetails = cartService.addBooksQuantityInCart(token, bookId,bookQuantityDetails);
+	    return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("Count of book in cart", 200,cartdetails));   	
+		
+	}
+	
 
 }
