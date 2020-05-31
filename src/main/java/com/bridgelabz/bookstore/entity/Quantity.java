3999df34b5ee170f9bdd7b506c5a9415837e605f
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-05-31 23:24:40 +0100

    [Sandeep].refactored

--- src/main/java/com/bridgelabz/bookstore/entity/Quantity.java
+++ src/main/java/com/bridgelabz/bookstore/entity/Quantity.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.bookstore.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,9 +10,11 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table
-public class Quantity 
+public class Quantity implements Serializable
 {
-    @Id
+   
+	private static final long serialVersionUID = 1L;
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long quantity_id;
     @Column
