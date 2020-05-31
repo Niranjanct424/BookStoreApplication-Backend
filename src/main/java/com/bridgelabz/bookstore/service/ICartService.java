ef83435e69c2f9d481d9647028a802edad80f8e6
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-05-31 19:39:09 +0100

    [Sandeep].deleted code

--- src/main/java/com/bridgelabz/bookstore/service/ICartService.java
+++ src/main/java/com/bridgelabz/bookstore/service/ICartService.java
@@ -6,5 +6,6 @@ import com.bridgelabz.bookstore.entity.CartItem;
 
 public interface ICartService {
  List<CartItem> addBooktoCart(String token,long bookId);
+ List<CartItem> getBooksfromCart(String token);
  
 }
