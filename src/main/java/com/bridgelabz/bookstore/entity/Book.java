89987cf24d796aaa02f2fbbdc17de26b936a8901
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-06-02 12:23:40 +0100

    [Sandeep].added rating  mapped in book

--- src/main/java/com/bridgelabz/bookstore/entity/Book.java
+++ src/main/java/com/bridgelabz/bookstore/entity/Book.java
@@ -2,11 +2,14 @@ package com.bridgelabz.bookstore.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -31,12 +34,8 @@ public class Book implements Serializable{
 	private LocalDateTime updatedDateAndTime;
 	private String status;
 	private String image;
-
-
-
-
-	
-	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Rating> reviewRating;
 
 
 }
