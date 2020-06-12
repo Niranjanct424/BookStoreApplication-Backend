a2e65f8f644aae55f4372a9e8aee00e8707311ba
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-06-12 12:30:26 +0100

    [Sandeep].add refactored adress dto

--- src/main/java/com/bridgelabz/bookstore/dto/UpdateAddressDto.java
+++ src/main/java/com/bridgelabz/bookstore/dto/UpdateAddressDto.java
@@ -4,21 +4,16 @@ import lombok.Data;
 
 @Data
 public class UpdateAddressDto {
-	private String address;
+	
 	private long addressId;
-	private String houseNo;
- 
-	private String street;
 
+	private String address;
+	private String locality;
+	private String name;
 	private String city;
-
 	private String landmark;
-
 	private String state;
-
 	private String pincode;
-
 	private String country;
-
-	private String type;
+	private String phoneNumber;
 }
